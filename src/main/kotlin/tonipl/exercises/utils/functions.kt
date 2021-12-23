5c58cd15181b04e5233596a2a6ca2170a0eade84
tonipl <t.polo.lopez@gmail.com>
2021-12-23 10:11:55 +0100

    Best solution.

--- src/main/kotlin/tonipl/exercises/utils/functions.kt
+++ src/main/kotlin/tonipl/exercises/utils/functions.kt
@@ -1,17 +1,5 @@
 package tonipl.exercises.utils
 
 fun solution(str: String): Int {
-    val listOfVowels = listOf("a", "e", "i", "o", "u")
-
-    // String is converted to a String array so that it can be compared to each String vowel:
-    val strArray: Array<String> = str.toCharArray().map { it.toString() }.toTypedArray()
-
-    var count = 0
-    for (vowel in listOfVowels) {
-        for (character in strArray)
-            if (character == vowel) {
-                count++
-            }
-    }
-    return count
+    return str.count { it in "aeiou" }
 }
