2e3d2917080864c4c5ac7612719e5f14f1497f5b
tonipl <t.polo.lopez@gmail.com>
2022-01-15 09:52:00 +0100

    Best solution.

--- src/main/kotlin/tonipl/exercises/utils/functions.kt
+++ src/main/kotlin/tonipl/exercises/utils/functions.kt
@@ -1,17 +1,5 @@
 package tonipl.exercises.utils
 
 fun solution(pp0: Int, percent: Double, aug: Int, p: Int): Int {
-    val newPercent = percent/100
-    var newPopulation = pp0
-
-    newPopulation = (newPopulation + newPopulation * newPercent + aug).toInt()
-
-    var count = 1
-    if (newPopulation < p) {
-        while (newPopulation < p) {
-            count++
-            newPopulation = (newPopulation + newPopulation * newPercent + aug).toInt()
-        }
-    }
-    return count
+    return generateSequence(pp0){ it + (percent/100.0 * it).toInt() + aug}.indexOfFirst { it >= p }
 }
