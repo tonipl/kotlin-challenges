b2f4214ba2590ad2eb621d4ec71252540fc0d463
tonipl <t.polo.lopez@gmail.com>
2022-01-15 10:04:20 +0100

    Best solution.

--- src/main/kotlin/tonipl/exercises/utils/functions.kt
+++ src/main/kotlin/tonipl/exercises/utils/functions.kt
@@ -1,22 +1,16 @@
 package tonipl.exercises.utils
 
+
 object RemovedNumbers {
     fun solution(n: Long): Array<LongArray> {
         val result = mutableListOf<LongArray>()
         val sequence = LongArray(n.toInt()) { it.toLong() + 1 }
-        val total = sequence.sum()
+        val sumTotal = sequence.sum()
 
-        sequence.forEachIndexed  { i, firstValue ->
-            sequence.forEachIndexed  { j, secondValue ->
-                if (i != j) {
-                    val sum = total - firstValue - secondValue
-                    val product = firstValue * secondValue
-
-                    if (sum == product) {
-                        val pair = longArrayOf(firstValue, secondValue)
-                        result.add(pair)
-                    }
-                }
+        sequence.forEach { value ->
+            val y = (sumTotal - value) / (value + 1)
+            if (y <= n && value * y == sumTotal - value - y) {
+                result.add(longArrayOf(value, y))
             }
         }
         return result.toTypedArray()
