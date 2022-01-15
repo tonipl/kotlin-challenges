37f1fb3e6e95cf676ed5927ecabdb92ffe49da61
tonipl <t.polo.lopez@gmail.com>
2022-01-15 09:41:41 +0100

    Best solution.

--- src/main/kotlin/tonipl/exercises/utils/functions.kt
+++ src/main/kotlin/tonipl/exercises/utils/functions.kt
@@ -1,15 +1,5 @@
 package tonipl.exercises.utils
 
 fun solution(ages: List<Int>): List<Int> {
-    var twoOldestNumbers = mutableListOf(0, 0)
-
-    for (age in ages) {
-        for ((index, oldestNumber) in twoOldestNumbers.withIndex()) {
-            if (age > oldestNumber) {
-                twoOldestNumbers = twoOldestNumbers.sorted() as MutableList<Int>
-                twoOldestNumbers[index] = age
-            }
-        }
-    }
-    return twoOldestNumbers.sorted()
+    return ages.sorted().takeLast(2)
 }
